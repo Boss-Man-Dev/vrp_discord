6ba598487913534888ed1cd43684dc33c657c508
Boss_Man <54071671+Boss-Man-Dev@users.noreply.github.com>
2023-06-28 10:27:44 -0500

    Update server.lua
    optimized event calls and added extendability

--- server.lua
+++ server.lua
@@ -19,69 +19,36 @@ function Discord:sendMsg(source, url, name, message)
 	PerformHttpRequest(url, function(err, text, headers) end, 'POST', json.encode(discordPayload), { ['Content-Type'] = 'application/json' })
 end
 
+function Discord:events(self, user, event)
+	local identity = vRP.EXT.Identity:getIdentity(user.cid)
+	if identity then
+		local name = identity.firstname .. " " .. identity.name
+		local message
+		if event == "join" then
+			message = name .. " joined."
+		elseif event == "death" then
+			message = name .. " died."
+		elseif event == "leave" then
+			message = name .. " left."
+		end
+		self:sendMsg(user.source, self.cfg.channels.newsFeed.webhook, 'SYSTEM', message)
+	end
+end
+
 Discord.event = {}
 
 function Discord.event:playerSpawn(user, first_spawn)
 	if first_spawn then
-		local identity = vRP.EXT.Identity:getIdentity(user.cid)
-		local name = identity.firstname .. " " .. identity.name
-
-		self:sendMsg(user.source, self.cfg.channels.newsFeed.webhook, 'SYSTEM', name .. ' joined.')
+		self:events(self, user, "join")
 	end
 end
 
 function Discord.event:playerDeath(user)
-	local identity = vRP.EXT.Identity:getIdentity(user.cid)
-	local name = identity.firstname .. " " .. identity.name
-
-	self:sendMsg(user.source, self.cfg.channels.newsFeed.webhook, 'SYSTEM', name .. ' died.')
+	self:events(self, user, "death")
 end
 
 function Discord.event:playerLeave(user)
-	local identity = vRP.EXT.Identity:getIdentity(user.cid)
-	local name = identity.firstname .. " " .. identity.name
-
-	self:sendMsg(user.source, self.cfg.channels.newsFeed.webhook, 'SYSTEM', name .. ' left.')
+	self:events(self, user, "leave")
 end
-
--- cfx 
-
---[[
-local function sendMsg(source, url, name, message)
-	local discordPayload = {
-		username = name,
-		content = message
-	}
-
-	PerformHttpRequest(url, function(err, text, headers) end, 'POST', json.encode(discordPayload), { ['Content-Type'] = 'application/json' })
-end
-
-function GetCharacterId(player)
-    local identifiers = GetPlayerIdentifiers(player)
-    for _, identifier in ipairs(identifiers) do
-        -- Assuming the character ID is stored in an identifier called "steam" or "license"
-        if string.find(identifier, "steam:") or string.find(identifier, "license:") then
-            local parts = split(identifier, ":")
-            local characterId = parts[2]
-            return characterId
-        end
-    end
-    return nil
-end
-
-AddEventHandler('playerSpawn', function(user, first_spawn)
-	if first_spawn then
-		sendMsg(user.source, self.cfg.channels.newsFeed.webhook, 'SYSTEM', name .. ' joined.')
-	end
-end)
-
-AddEventHandler('playerDeath', function(user)
-	sendMsg(user.source, self.cfg.channels.newsFeed.webhook, 'SYSTEM', name .. ' died.')
-end)
-
-AddEventHandler('playerDropped', function(user)
-	sendMsg(user.source, self.cfg.channels.newsFeed.webhook, 'SYSTEM', name .. ' left.')
-end)
-]]--
 
 vRP:registerExtension(Discord)                                   					
